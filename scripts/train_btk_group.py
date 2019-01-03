cc62b86d19b220ab7c4fa0c456596b9fc8df79ea
sowmyakth <sowmya.spacebug@gmail.com>
2019-01-03 13:54:25 -0800

    script to train ResidualDetectron with btk and with blends as groups added

---
+++ scripts/train_btk_group.py
@@ -0,0 +1,50 @@
+# Script to train data
+##use utils.generate_anchors_no_spill
+import os
+import sys
+import numpy as np
+import h5py
+import dill
+import pandas as pd
+
+
+# Root directory of the project
+ROOT_DIR = '/home/users/sowmyak/ResidualDetectron'
+# Directory to save logs and trained model
+MODEL_DIR = '/scratch/users/sowmyak/resid/logs'
+# path to images
+DATA_PATH = '/scratch/users/sowmyak/resid/data'
+CODE_PATH = '/home/users/sowmyak/ResidualDetectron/scripts'
+sys.path.append(CODE_PATH)
+import btk_utils
+# from mrcnn.model import log
+
+
+def main(Args):
+    """Test performance for btk input blends"""
+    count = 4000
+    catalog_name = os.path.join("/scratch/users/sowmyak/data", 'OneDegSq.fits')
+    resid_model = btk_utils.Resid_metrics_model(
+        Args.model_name, Args.model_path, MODEL_DIR, training=True,
+        new_model_name=Args.model_name + "_btk", images_per_gpu=8)
+    resid_model.make_resid_model(catalog_name, count=count,
+                                 max_number=2)
+    learning_rate = resid_model.config.LEARNING_RATE
+    history1 = resid_model.model.train(resid_model.dataset, None,
+                                       learning_rate=learning_rate,
+                                       epochs=80,
+                                       layers='all')
+    name = resid_model.config.NAME + '_run1_loss'
+    with open(name + ".dill", 'wb') as handle:
+        dill.dump(history1.history, handle)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str,
+                        help="Name of model to evaluate")
+    parser.add_argument('--model_path', type=str,
+                        help="Saved weights of model")
+    args = parser.parse_args()
+    main(args)
