ef861543f8a00b4cf8651bf34eff6d3f819dfc77
sowmyakth <sowmya.spacebug@gmail.com>
2019-01-10 15:20:15 -0800

    detection in i band, option for group blend sampling function added

--- scripts/compare_sep.py
+++ scripts/compare_sep.py
@@ -42,37 +42,72 @@ class Sep_params(btk.measure.Measurement_params):
         return [None, peaks]
 
 
-def get_btk_generator():
-    """Returns btk.measure generator for default settings"""
+class Sep_params_i_band(btk.measure.Measurement_params):
+    """Class containing functions to perform detection"""
+    def get_centers(self, image):
+        """Returns x and y coordinates of object centroids detected by sep.
+        Detection is performed in the i band.
+        Args:
+            image: multi-band image to perform detection on. ([#bands, x, y])
+        Returns:
+            x and y coordinates of detected centroids.
+        """
+        detect = image[3]  # detection in i band
+        bkg = sep.Background(detect)
+        catalog = sep.extract(detect, 1.5, err=bkg.globalrms)
+        return np.stack((catalog['x'], catalog['y']), axis=1)
+
+    def get_deblended_images(self, data, index):
+        """Returns detected centers for the given blend
+        Args:
+            data: output from btk.draw_blends generator
+            index: index of blend in bacth_outputs to perform analysis on.
+        Returns:
+            deblended images and detected centers
+        """
+        image = np.transpose(data['blend_images'][index], axes=(2, 0, 1))
+        peaks = self.get_centers(image)
+        return [None, peaks]
+
+
+def get_btk_generator(meas_params, max_number, sampling_function,
+                      selection_function, wld_catalog):
+    """Returns btk.measure generator for input settings
+
+    """
     # Input catalog name
     catalog_name = os.path.join("/scratch/users/sowmyak/data", 'OneDegSq.fits')
     # Load parameters
     param = btk.config.Simulation_params(
-        catalog_name, max_number=2, batch_size=1, seed=199)
+        catalog_name, max_number=max_number, batch_size=8, stamp_size=25.6)
+    if wld_catalog:
+            param.wld_catalog = wld_catalog
     np.random.seed(param.seed)
     # Load input catalog
-    catalog = btk.get_input_catalog.load_catlog(param)
+    catalog = btk.get_input_catalog.load_catlog(
+        param, selection_function=selection_function)
     # Generate catalogs of blended objects
     blend_generator = btk.create_blend_generator.generate(
-        param, catalog, btk_utils.resid_general_sampling_function)
+        param, catalog, sampling_function)
     # Generates observing conditions for the selected survey_name & all bands
     observing_generator = btk.create_observing_generator.generate(
         param, btk_utils.resid_obs_conditions)
     # Generate images of blends in all the observing bands
     draw_blend_generator = btk.draw_blends.generate(
         param, blend_generator, observing_generator)
-    meas_params = Sep_params()
     meas_generator = btk.measure.generate(
         meas_params, draw_blend_generator, param)
     return meas_generator
 
 
-def run_sep():
+def run_sep(test_size, max_number, sampling_function,
+            selection_function=None, wld_catalog=None):
     """Test performance for btk input blends"""
-    meas_generator = get_btk_generator()
+    meas_generator = get_btk_generator(
+        max_number, sampling_function, selection_function, wld_catalog)
     results = []
-    np.random.seed(0)
-    for im_id in range(4000):
+    np.random.seed(1)
+    for im_id in range(test_size):
         output, deb, _ = next(meas_generator)
         blend_list = output['blend_list'][0]
         detected_centers = deb[0][1]
